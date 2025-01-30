92306fab8207b29b6bb5516b791025228a42d69d
meganhall719 <megan.hall@utexas.edu>
2025-01-30 17:04:52 -0600

    go to git

--- 1.30.25  Class Notes.R
+++ 1.30.25  Class Notes.R
@@ -1,3 +1,6 @@
+df <- read.csv(file + "~/Desktop/CPDS-1960-2014-reduced.csv, sep = ",", header = TRUE, stringsAsFactors = FALSE")
+head(df)
+tail(df)
 library(tidyverse)
 f<- file.choose()
 df<- read_csv(file = f, col_names = TRUE)
@@ -7,3 +10,23 @@ df <- read_csv(file = f, col_names = TRUE)
 head(df)
 class(df)
 str(df)
+
+f<-file.choose()
+df <- read_csv(f, col_names = TRUE)
+df$density <- df$population/d$area
+v <- sort(df$density,decreasing = TRUE)
+df <- arrange(df, desc(density))
+head d, 
+
+library(tidyverse)
+d <- read_csv(f, col_names = TRUE)
+d$density <- d$population/d$area
+v<- sort(d$density, decreasing = TRUE)
+d <- d%>%
+  arrange(desc(density))
+str(d,10)
+
+#piping 
+d <- d%>%
+structure(d)
+d<-read.csv(f, header = TRUE)
